0413ace105ef6bb5eee186bc5dd14fdb7868fa46
andrey <andrey.delbello2002@gmail.com>
2022-11-07 11:01:15 -0300

    Progresso atual

---
+++ php_estudos_curso-main/5_op_e_expressores/6_exercicio_12/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// +,-,/,*
+  $a = 10;
+  $b = 3;
+  $op1 = $a - $b;
+  echo $op1 . "<br>";
+
+  $c = 12.4;
+  $op2 = $op1 * $c;
+  echo $op2. "<br>";
+
+  $d = 4.8;
+  $op3 = $op2 + $d;
+  echo $op3. "<br>";
+
+  $e = 4;
+  $op4 = $op3 / $e;
+  echo $op4. "<br>";
+  

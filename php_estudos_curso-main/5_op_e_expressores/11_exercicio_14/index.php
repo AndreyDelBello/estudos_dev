b23157fdb7598440eb115b25b86f3aa201e4edd5
andrey <andrey.delbello2002@gmail.com>
2022-11-08 09:13:45 -0300

    Atualização de progresso

---
+++ php_estudos_curso-main/5_op_e_expressores/11_exercicio_14/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  $saudacao = "Sr.";
+  $nome = "Andrey";
+  $sobrenome = "Del Bello";
+
+  $frase = "O" . " " . $saudacao . " " . $nome . " " . $sobrenome . " terá uma reunião às 14 horas.";
+  echo $frase;
+  echo "<br>";
---
+++ php_estudos_curso-main/5_op_e_expressores/8_exercicio_13/index.php
@@ -0,0 +1,8 @@
+<?php
+  // --> % Resto da divisão;
+  $a = 14;
+  $b = 2;
+  $c = 3;
+
+  echo $a % $b . "<br>";
+  echo $a % $c. "<br>";

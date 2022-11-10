425133ddb1db6e5c567516cb9aeaeead3ea8257e
andrey <andrey.delbello2002@gmail.com>
2022-11-10 10:59:05 -0300

    Progresso do curso

---
+++ php_estudos_curso-main/5_op_e_expressores/27_exercicio_19/index.php
@@ -0,0 +1,18 @@
+<?php
+
+  $a = (int) "testando";
+  echo $a . "<br>";
+  $b = (int) "asd";
+  echo $b . "<br>";
+
+  $c = (int) 12.9;
+  echo $c. "<br>";
+
+  $d = (int) true;
+  echo $d. "<br>";
+
+  $e = (int) false;
+  echo $e. "<br>";
+
+  $f = (int) [1,2,3];
+  echo $f . "<br>";

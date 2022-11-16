a00e304a8b040f4f66480650a3e0948b07fa7399
andrey <andrey.delbello2002@gmail.com>
2022-11-16 07:44:08 -0300

    Progresso Atual

--- php_estudos_curso-main/6_estruturas_de_controle/2_exercicio_21/index.php
+++ php_estudos_curso-main/6_estruturas_de_controle/2_exercicio_21/index.php
@@ -4,17 +4,17 @@
   $b = 2;
 
   if($a > $b) {
-    echo "Entrou no if, parametro verdadeiro.<br>";
+    echo "Entrou no if, parametro verdadeiro 1<br>";
   }
 
   $c = "Matheus";
   $d = "Pedro";
   if($c != $d) {
-    echo "Entrou no if, parametro verdadeiro.<br>";
+    echo "Entrou no if, parametro verdadeiro 2<br>";
   }
 
   $e = 12;
   $f = 11;
   if($e <= $f) {
-    echo "Entrou no if, parametro verdadeiro.<br>";
+    echo "Entrou no if, parametro verdadeiro 3<br>";
   }
